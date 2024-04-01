1decb2e360b3cba48e2fa631f23180f28b8b969b
EniodaS <frank.sadoine@yahoo.de>
2024-04-01 20:34:47 +0200

    created visual login screen

--- app/src/main/java/de/max/trailblazerfrontendv1/MainActivity.kt
+++ app/src/main/java/de/max/trailblazerfrontendv1/MainActivity.kt
@@ -3,11 +3,14 @@ package de.max.trailblazerfrontendv1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 //import dagger.hilt.android.AndroidEntryPoint
@@ -20,30 +23,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TrailBlazerFrontendV1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MapScreen()
-                }
+                Greeting("android")
             }
+
+//            TrailBlazerFrontendV1Theme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    MapScreen()
+//                }
+//            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface{
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:Nexus One", showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TrailBlazerFrontendV1Theme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true, device = "id:Nexus One", showSystemUi = true)
+@Composable
+fun GreetingPreviewDark() {
+    TrailBlazerFrontendV1Theme(darkTheme = true) {
         Greeting("Android")
     }
 }
