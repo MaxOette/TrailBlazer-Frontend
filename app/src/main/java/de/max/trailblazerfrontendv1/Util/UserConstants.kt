90fca17accb3d179088523dcd576698f36a3a016
Max <i21001@hb.dhbw-stuttgart.de>
2024-04-15 23:53:54 +0200

    try stuff that still doesnt work 🙃

--- app/src/main/java/de/max/trailblazerfrontendv1/Util/UserConstants.kt
+++ app/src/main/java/de/max/trailblazerfrontendv1/Util/UserConstants.kt
@@ -1,12 +1,16 @@
 package de.max.trailblazerfrontendv1.Util
 
- class UserConstants {
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+
+class UserConstants {
      companion object {
          var refreshToken: String = ""
          var accessToken: String = ""
          var email: String = ""
          var userLat: Double = 48.8092627
          var userLng: Double = 8.9775588
+         var cameraPosition = CameraPosition.fromLatLngZoom(LatLng(userLat, userLng), 14f)
 
 //         fun getRefreshToken(): String{
 //             return refreshToken
--- app/src/main/java/de/max/trailblazerfrontendv1/location/DefaultLocationClient.kt
+++ app/src/main/java/de/max/trailblazerfrontendv1/location/DefaultLocationClient.kt
@@ -10,6 +10,8 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import de.max.trailblazerfrontendv1.Util.UserConstants
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -51,6 +53,7 @@ class DefaultLocationClient(
                         println(result.locations.size)
                         UserConstants.userLat = location.latitude
                         UserConstants.userLng = location.longitude
+                        UserConstants.cameraPosition = CameraPosition.fromLatLngZoom(LatLng(location.latitude, location.longitude), 14f)
                     }
                 }
             }
