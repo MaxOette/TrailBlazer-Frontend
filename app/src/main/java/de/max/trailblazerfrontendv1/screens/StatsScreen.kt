68ca493e161cc057ec0b4f46ba5c792ecebef637
Max <i21001@hb.dhbw-stuttgart.de>
2024-05-14 22:24:12 +0200

    start impl of StatsScreen

--- app/src/main/java/de/max/trailblazerfrontendv1/screens/StatsScreen.kt
+++ app/src/main/java/de/max/trailblazerfrontendv1/screens/StatsScreen.kt
@@ -1,44 +1,91 @@
 package de.max.trailblazerfrontendv1.screens
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import de.max.trailblazerfrontendv1.Api.TileApi
-import de.max.trailblazerfrontendv1.Api.TileData
-import de.max.trailblazerfrontendv1.Util.UserConstants
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import de.max.trailblazerfrontendv1.R
 
 @Composable
 fun StatsScreen() {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Stats Screen"
-        )
-        Button(
-            onClick = {
-                println("getTiles clicked --------------------")
-                GlobalScope.launch(Dispatchers.IO) {
-                    try {
-                        val response = TileApi.tileService.getTiles(UserConstants.userLat, UserConstants.userLng, 10)
-                        val tileData : List<TileData> = response
-                        UserConstants.testTileData = tileData
-                        println("" + tileData.size + " Kacheln geladen.")
-                        println(tileData)
-                    }   catch(e: Error) {
-                        println(e.message)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 30.dp)
+        ) {
+            Text(
+                text = "Stats",
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            LazyColumn {
+                items(16) { index ->
+                    val bundesland = "Bundesland $index"
+                    val prozentzahl = (1..100).random()
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 10.dp
+                        )
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(16.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.bb),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(48.dp)
+                                    .clip(CircleShape)
+                                    .background(MaterialTheme.colorScheme.primary)
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Column {
+                                Text(text = bundesland, fontWeight = FontWeight.Bold)
+                                Spacer(modifier = Modifier.height(8.dp))
+                                LinearProgressIndicator(
+                                    progress = prozentzahl / 100f,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = "$prozentzahl%", fontWeight = FontWeight.Bold)
+                            }
+                        }
                     }
                 }
             }
-        ){
-            Text( text = "get Tiles")
         }
     }
 }
